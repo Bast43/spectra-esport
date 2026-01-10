d374546a2b1eb2d7096fef006f44b2667be894e7
Bast43 <coduti.bastien43@gmail.com>
2026-01-10 05:37:11 +0100

    Fix: Enable Twitter widget with CSP headers

--- next.config.js
+++ next.config.js
@@ -3,6 +3,28 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: [
+              "default-src 'self'",
+              "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://platform.twitter.com https://cdn.syndication.twimg.com",
+              "style-src 'self' 'unsafe-inline' https://platform.twitter.com",
+              "img-src 'self' data: https: http:",
+              "font-src 'self' data:",
+              "connect-src 'self' https://api.twitter.com https://syndication.twitter.com",
+              "frame-src 'self' https://platform.twitter.com https://twitter.com https://syndication.twitter.com",
+              "media-src 'self' https:",
+            ].join('; '),
+          },
+        ],
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
