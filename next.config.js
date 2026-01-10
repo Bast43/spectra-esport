3884c25ecbf9628b8f89e46ee2f066da81b3cb4e
Bast43 <coduti.bastien43@gmail.com>
2026-01-10 15:37:07 +0100

    Enable Twitter widget

--- next.config.js
+++ next.config.js
@@ -3,28 +3,6 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  async headers() {
-    return [
-      {
-        source: '/:path*',
-        headers: [
-          {
-            key: 'Content-Security-Policy',
-            value: [
-              "default-src 'self'",
-              "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://platform.twitter.com https://cdn.syndication.twimg.com",
-              "style-src 'self' 'unsafe-inline' https://platform.twitter.com https://fonts.googleapis.com",
-              "img-src 'self' data: https: http:",
-              "font-src 'self' data: https://fonts.gstatic.com",
-              "connect-src 'self' https://api.twitter.com https://syndication.twitter.com",
-              "frame-src 'self' https://platform.twitter.com https://twitter.com https://syndication.twitter.com",
-              "media-src 'self' https:",
-            ].join('; '),
-          },
-        ],
-      },
-    ]
-  },
 }
 
 module.exports = nextConfig
